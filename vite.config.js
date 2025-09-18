4ae15a119bd896d9c3105cc182414833a492f492
Çetin Karasar <cetinKarasar@users.noreply.github.com>
2025-09-18 17:53:19 +0300

    Add allowedHost for Netlify Visual Editor

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     allowedHosts: [
-      "localhost",
-      "devserver-preview--silver-sunshine-3f5578.netlify.app"
-    ]
-  }
+      '.netlify.app',
+      'devserver-preview--silver-sunshine-3f5578.netlify.app'
+    ],
+  },
 });
